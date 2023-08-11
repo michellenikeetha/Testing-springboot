6370045012e98d2238ff88ef17fd15b1ed7443c5
Michelle Perera <michellenikeetha@gmail.com>
2023-08-12 00:38:20 +0530

    Delete tests directory

--- tests/src/main/java/com/example/tests/repo/MusicRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.tests.repo;
-
-import com.example.tests.entity.Music;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicRepo extends JpaRepository<Music, Integer> {
-}
