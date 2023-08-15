de0c8568b6db7e877b69589e17c31f05e12ac8e5
Michelle Perera <michellenikeetha@gmail.com>
2023-08-15 09:48:14 +0530

    Delete employee-springboot-react-backend directory

--- employee-springboot-react-backend/src/main/java/com/example/employeespringbootreactbackend/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeespringbootreactbackend.repository;
-
-import com.example.employeespringbootreactbackend.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
